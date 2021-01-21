c2708f322b79553970c73e005c4960474b1172db
weingxing <2451809588@qq.com>
2021-01-21 18:10:29 +0800

    dev-ing...

---
+++ src/main/java/com/oxygen/liveqrcode/LiveQrCodeApplication.java
@@ -0,0 +1,13 @@
+package com.oxygen.liveqrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveQrCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveQrCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxygen/liveqrcode/LiveQrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxygen.liveqrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveQrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
